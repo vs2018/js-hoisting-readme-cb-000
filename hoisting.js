cadc205014533589cf51767b73db123d31db6485
Vishal Sharma <vsh1819@gmail.com>
2018-08-03 08:04:00 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -23,10 +23,12 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+
+  sayMy();
+
+
 }
